9bf129a93843c20691f8b9e8ebe67c10a9da5e86
cheikhwade07 <seydicheikhwade@gmail.com>
2025-01-10 20:25:58 -0500

    Add files via upload

--- youtube_downloader_github_ver/spotipy_Github_function.py
+++ youtube_downloader_github_ver/spotipy_Github_function.py
@@ -1,7 +1,12 @@
-import spotipy 
-from spotipy.oauth2 import SpotifyOAuth 
+try:
+    import spotipy 
+    from spotipy.oauth2 import SpotifyOAuth 
+except:
+      print("spotipy is not installed go to your command prompt and insert pip install spotipy or search for another way to install it")
+      quit()
 
-def show_tracks(results:dict):
+
+def show_tracks(results:dict):#show tracks of one of your spotify playlist
     #itterate over results which is a playlist in this context
     for i, item in enumerate(results['items']):
         track = item['track']
@@ -9,11 +14,15 @@ def show_tracks(results:dict):
             "   %d %32s %s" %
             (i+1, track['artists'][0]['name'], track['name']))
 
-def spotifify_identification():
+def spotifify_identification():#!!!!! Take care of the identification process
      scope = 'playlist-read-private'
+     """
+     To use this app you need to go to https://developer.spotify.com/dashboard create an app use http://localhost:8888/callback as the redirect url 
+     and copy and paste client_id and client_secret in the following source 
+     """
      sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
-            client_id="afc1597614d04a66943024dd80fed279",
-            client_secret="7aae355ec6c34760b3080259a424b93b",
+            client_id="",
+            client_secret="",
             redirect_uri="http://localhost:8888/callback",
             scope=scope
             ))
@@ -38,7 +47,7 @@ def select_a_playlist()->dict:
      playlists = sp.current_user_playlists()
      user_id = sp.me()['id']
      playlist_list=[]
-     for playlist in playlists['items']:
+     for playlist in playlists['items']:#make a list contaning all your playlist name
                 playlist_list+=[playlist['name']]
      print(playlist_list)
      select= input("Which playlist do you want to select: ")
